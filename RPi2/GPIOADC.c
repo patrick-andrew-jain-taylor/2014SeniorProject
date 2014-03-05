50132eb98901b17e4e2f11b2c9e1633297da9313
Patrick Taylor <trickyconverse@gmail.com>
2014-03-04 19:25:08 -0500

    Clock function temporarily written for functionality

--- RPi2/GPIOADC.c
+++ RPi2/GPIOADC.c
@@ -6,7 +6,7 @@
 void GPIOADC{
 	wiringPiSetup();
 	//set modes
-	pinMode(7, GPIO_CLOCK); // GPIO 4  -- HSYNC0
+	pinMode(7, OUTPUT);	// GPIO 4  -- HSYNC0
 	pinMode(0, INPUT);	// GPIO 17 -- HSOUT
 	pinMode(1, OUTPUT);	// GPIO 18 -- VSYNC0
 	pinMode(2, INPUT);	// GPIO 21 -- VSOUT
@@ -14,14 +14,34 @@ void GPIOADC{
 	pinMode(4, INPUT);	// GPIO 23 -- EXTCLK
 	pinMode(5, OUTPUT);	// GPIO 24 -- CLAMP
 	pinMode(6, OUTPUT);	// GPIO 25 -- COAST
-	//set function
-	gpioClockSetWPi(7, 31500); //set HSYNC to 31.5KHz
+	//set clocks
+	digitalWrite(7, HIGH);
+	digitalWrite(1, HIGH);
+	//initalize counters
+	int hcount = 0; //used to generate 60Hz*480 clock
+	int vcount = 0; //used to generate 60Hz clock
 	//basic implementation of 60Hz clock -- NOT FINAL
-	while(1){
-		digitalWrite(1, HIGH); 
-		delayMicroseconds(16667);
-		digitalWrite(0, LOW);
-		delayMicroSeconds(16667);
+	while(1){//loop indefinitely -- not final implementation
+		//Check valid data first
+		if (hcount < 640){ //0-639 valid data
+			if (vcount < 480){ //0-479 valid data
+				//tell RPI1 to inc mux
+			}
+		}
+		//HSYNC first
+		//@660 -> HSYNC set low
+		if (hcount == 660) digitalWrite(7, LOW);
+		//@756 -> HSYNC set high
+		else if (hcount == 756) digitalWrite(7, HIGH);
+		else if (hcount == 800) {
+			//VSYNC Second
+			//@494 -> VSYNC set low
+			if (vcount == 494) digitalWrite(1, LOW);
+			//@495 -> VSYNC set high
+			else if (vcount == 495) digitalWrite(1, HIGH);
+			vcount = (vcount++) % 525; //increment VSYNC < 525
+		}
+		hcount = (hcount++ % 800); //increment HSYNC < 800
 	}
 	//no need for other functions yet -- will add additional code if the 
 	//case	
