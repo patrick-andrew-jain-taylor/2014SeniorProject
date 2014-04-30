29ddfc82292e86e138db60d7533789ddf004ce93
Patrick Taylor <trickyconverse@gmail.com>
2014-04-30 13:48:46 -0400

    Individual frame and total GIF parsed using CSV library.

--- RPi1/csvexample.c
+++ RPi1/csvexample.c
@@ -2,29 +2,44 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "libcsv/csv.h"
 
 struct counts {
-	long unsigned fields;
-	long unsigned rows;
+	int fields;
+	int rows;
+	int frameCount;
+	uint16_t frame[160][120];
 };
 
 void cb1 (void *s, size_t len, void *data) {
-	((struct counts *)data)->fields++; }
+	((struct counts *)data)->frame
+		[((struct counts *)data)->fields%160][((struct counts *)data)->rows%120] = atoi(s);
+	printf("%u\t", 
+		((struct counts *)data)->frame
+			[((struct counts *)data)->fields%160][((struct counts *)data)->rows%120]);
+	((struct counts *)data)->fields++;
+	}
 void cb2 (int c, void *data) {
-	((struct counts *)data)->rows++; }
+	printf("\n");
+	((struct counts *)data)->rows++;
+	if (((struct counts *)data)->rows%120 == 0) printf("Frame %d\n", ((struct counts *)data)->frameCount++); 
+	}
+
+
 	
 int main (int argc, char *argv[]) {
 	FILE *fp;
 	struct csv_parser p;
 	char buf[1024];
 	size_t bytes_read;
-	struct counts c = {0, 0};
+	uint16_t frame[160][120];
+	struct counts c = {0, 0, 0};
 	
 	if (csv_init(&p, 0) != 0) exit(EXIT_FAILURE);
 	fp = fopen(argv[1], "rb");
 	if (!fp) exit(EXIT_FAILURE);
-	
+		
 	while ((bytes_read=fread(buf, 1, 1024, fp)) > 0)
 		if (csv_parse(&p, buf, bytes_read, cb1, cb2, &c) != bytes_read) {
 			fprintf(stderr, "Error while parsing file: %s\n",
@@ -35,7 +50,7 @@ int main (int argc, char *argv[]) {
 	csv_fini(&p, cb1, cb2, &c);
 	
 	fclose(fp);
-	printf("%lu fields, %lu rows\n", c.fields, c.rows);
+	printf("%d fields, %d rows\n", c.fields, c.rows);
 	
 	csv_free(&p);
 	exit(EXIT_SUCCESS);
